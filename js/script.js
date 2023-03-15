e991a4f7dc242adb7a69a986a742cd02696692c5
Gabriel Barbosa Moreira <109036024+GabrielMoreiraB@users.noreply.github.com>
2023-03-15 18:12:43 -0300

    .

--- js/script.js
+++ js/script.js
@@ -1,14 +1,22 @@
 import ehumcpf from "./valida-cpf.js";
+import  ehMaiorDeIdade  from "./valida-idade.js";
 
 const camposDoFormulario = document.querySelectorAll("[required]")
 
 
 camposDoFormulario.forEach(campo => {
     campo.addEventListener("blur", () => verificaCampo(campo));
+    campo.addEventListener("invalid", evento => evento.preventDefault())
 })
 
 function verificaCampo(campo){
     if(campo.name == 'cpf' && campo.value.length >=11){
         ehumcpf(campo)
     }
+
+    if(campo.name == 'aniversario' && campo.value !== ""){
+        ehMaiorDeIdade(campo)
+    }
+
+    console.log(campo.validity)
 }
